0f5eccc140c428bb90e026bcb2381df61c92302c
Lachlan Kendall <lachlan@Lachlans-MBP.gateway>
2017-02-23 21:58:01 +1100

    Added PV

--- renderer.js
+++ renderer.js
@@ -21,8 +21,25 @@ module.exports = AsgardLogin = function AsgardLogin() {
     driver.switchTo().frame('innerFrame');
     driver.findElement(By.name('username')).sendKeys(Config.username);
     driver.findElement(By.name('password')).sendKeys(Config.password);
-    // driver.findElement(By.className(' form-submit art-button buttonFont')).click();
-    // driver.wait(until.elementIsVisible(driver.findElement({ id: 'footerLinks' })), 5000);
+    driver.findElement(By.className(' form-submit art-button buttonFont')).click();
+    driver.wait(until.elementIsVisible(driver.findElement({ id: 'footerLinks' })), 5000);
+    
+    // Find interest rate 
+    // driver.findElement(By.id('searchSelect')).sendKeys('P');
+    // driver.findElement(By.name('searchText')).sendKeys(Config.product);
+    // driver.findElement(By.name('searchClient')).click();
+    // driver.findElement(By.id('Asgard eWRAP Super Account (AEWS) - Transfers')).click();
+
+    // Access selected client's PV 
+    driver.findElement(By.name('searchText')).sendKeys(Config.account);
+    driver.findElement(By.name('searchClient')).click();
+    driver.findElement(By.id('VIEW')).click();
+    driver.findElement(By.id('PORTFOLIO_VALUATION_AQT')).click();
+    // Print PV here
+
+    // Move to RoR of client
+    driver.findElement(By.id('account_enquiry_menu_select')).sendKeys('R');
+
     // driver.quit();
     console.log("in Asgard function");
 }
